cfe357d748b527e6ee1a7667f184f7b1361a1d91
Alexey Vinogradov <vinogradovai@bcs.ru>
2019-09-06 15:11:15 +0700

    #1 Skeleton of the project

---
+++ src/test/java/com/aivinog1/cardpay/AppTest.java
@@ -0,0 +1,22 @@
+package com.aivinog1.cardpay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTest {
+
+    @Autowired
+    private App app;
+
+    @Test
+    public void verifyThatRun() {
+        assertNotNull(app);
+    }
+}
