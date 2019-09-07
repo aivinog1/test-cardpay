ad7c2b6a5f3a3c69481c955872fc0bc99fddfdd9
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2019-09-07 12:42:29 +0700

    #17 Remove useless method

--- src/main/java/com/aivinog1/cardpay/parse/ConverterService.java
+++ src/main/java/com/aivinog1/cardpay/parse/ConverterService.java
@@ -3,18 +3,14 @@ package com.aivinog1.cardpay.parse;
 import com.aivinog1.cardpay.convert.Response;
 import com.aivinog1.cardpay.convert.SupportedFileType;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * @author aivinog1
  * @since 06.09.2019
  */
 public interface ConverterService {
-
-    CompletableFuture<List<Response>> convert(CompletableFuture<Path> file);
 
     List<Response> convert(Path file);
 
