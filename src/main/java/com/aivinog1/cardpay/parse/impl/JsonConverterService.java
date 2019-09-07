eda04bd22c9329de59bb6b9cc4b759d7876c2585
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2019-09-07 12:42:29 +0700

    #17 Remove useless method

--- src/main/java/com/aivinog1/cardpay/parse/impl/JsonConverterService.java
+++ src/main/java/com/aivinog1/cardpay/parse/impl/JsonConverterService.java
@@ -1,16 +1,13 @@
 package com.aivinog1.cardpay.parse.impl;
 
+import com.aivinog1.cardpay.convert.Response;
 import com.aivinog1.cardpay.convert.SupportedFileType;
 import com.aivinog1.cardpay.parse.ConverterService;
-import com.aivinog1.cardpay.convert.Response;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 /**
  * @author aivinog1
@@ -18,20 +15,6 @@ import java.util.concurrent.ExecutionException;
  */
 @Service
 public class JsonConverterService implements ConverterService {
-    /**
-     * @param file - a file that we need to read
-     * @return future that contains list of the responses.
-     * @todo #4:30m Let's implement this method. This method should return List of the future responses readed from JSONs.
-     * @todo #3:30m Let's remove this method and all of it's usages. It must be replaced by {@link JsonConverterService#convert(Path)}
-     */
-    @Override
-    public CompletableFuture<List<Response>> convert(CompletableFuture<Path> file) {
-        try {
-            return CompletableFuture.completedFuture(convert(file.get()));
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     /**
      * @param file - a file that needs to be readed
